12501f1eae5b90a19d9f0a1aef8786545b8fba85
Isaay Sosa Hernández <93836824+iso11ml@users.noreply.github.com>
2024-08-22 08:56:57 -0600

    Added console.log

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT);
+  const port = process.env.PORT || 8080;
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
 }
 bootstrap();
